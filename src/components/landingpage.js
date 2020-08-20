df998c87b9d4474f46d05e7a1241ee069832a878
Damiano Del Bello <dam6dam@gmail.com>
2020-08-20 17:54:54 +0200

    navbar ready

---
+++ src/components/landingpage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+class Landing extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Welcome to JavaScript</h1>
+                <h3>Don't be too serious :)</h3> 
+            </div>
+        )
+    }
+}
+
+export default Landing;
---
+++ src/components/main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import LandingPage from './landingpage.js';
+import AboutMe from './aboutme.js';
+import Contact from './contact';
+import Project from './projects';
+import Resume from './resume';
+import Projects from './projects';
+
+const Main = () => {
+    
+    return (
+        <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/aboutme" component={AboutMe} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/resume" component={Resume} />
+        </Switch>
+    )
+    
+}
+
+export default Main;
