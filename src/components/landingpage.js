86d974d425736188366c08ff9259708c51f2ac1b
Damiano Del Bello <dam6dam@gmail.com>
2020-08-21 11:09:00 +0200

    social icons + center alignment

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -6,7 +6,7 @@ class Landing extends Component {
     render() {
         return(
             <div style={{width: '100%', margin: 'auto'}}>
-                <Grid className="langing-grid">
+                <Grid className="landing-grid">
                     <Cell col={12}>
                         <img 
                             src="https://lh3.googleusercontent.com/proxy/6IYxfzz0c6zBbX3WspGwiA47MTwq2woemZgujZMRt_ocYzEnwc96_bFsA4I1ukSc-BESAqxNdJHeuSguDKqbokMV01CCMC9sUm-c6FzcIF6bGvdrGUirjQ"
@@ -18,6 +18,26 @@ class Landing extends Component {
 
                             <hr/>
                             <p>HTML/CSS | Bootstrap | JavaScript | React | Ruby | MongoDB</p>
+
+                            <div className="social-links">
+                                {/* LinkedIn */}
+                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                </a>
+                                {/* Github */}
+                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+                                {/* Freecodecamp */}
+                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-free-code-camp" aria-hidden="true" />
+                                </a>
+                                {/* Youtube */}
+                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-youtube-square" aria-hidden="true" />
+                                </a>
+
+                            </div>
                         </div>
                     </Cell>
                 </Grid>
